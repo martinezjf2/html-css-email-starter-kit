f0e04032fbd82490e8561ef71b2b5106bb1b4f09
Jeffrey Martinez <martinezjf2@gmail.com>
2024-09-18 15:07:20 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Run `gulp` or `npm run watch` or `npm start` to get the server running
+
+Run ` npm install --legacy-peer-deps` to install the dependencies
+
+Use node version 20.5.0 and npm version 9.8.0
---
+++ src/js/App.js
@@ -0,0 +1,105 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      {/* Start of HTML Email */}
+      <table selfspacing="0" selfpadding="0" width="100%" border="0">
+        <tr>
+          <td>
+            {/* Start of Container */}
+            <table
+              width="600px"
+              align="center"
+              selfspacing="0"
+              selfpadding="0"
+              border="0"
+              className="container"
+            >
+              {/* Start of Main Row */}
+              <tr>
+                <td>
+                  <table
+                    width="50%"
+                    align="left"
+                    selfspacing="0"
+                    selfpadding="0"
+                    border="0"
+                    className="container"
+                  >
+                    <tr>
+                      <td>Left</td>
+                    </tr>
+                  </table>
+                  <table
+                    width="50%"
+                    align="right"
+                    selfspacing="0"
+                    selfpadding="0"
+                    border="0"
+                    className="container"
+                  >
+                    <tr>
+                      <td>Right</td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+
+              {/* Start of Main Row */}
+              <tr>
+                <td></td>
+              </tr>
+              {/* End of Main Row */}
+            </table>
+            {/* End of Container */}
+          </td>
+        </tr>
+      </table>
+      {/* End of HTML Email */}
+    </div>
+  );
+}
